23763efbf240563c0922270721b949e494f704d8
mulungi-git <77009387+mulungi-git@users.noreply.github.com>
2023-07-05 02:59:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import numpy as np
 from io import BytesIO
 from PIL import Image
 import tensorflow as tf
+from tensorflow.keras.models import load_model
 
 app = FastAPI()
 
 origins = [
     "http://localhost",
-    "http://localhost:3000",
+    "http://localhost:3005",
 ]
 app.add_middleware(
     CORSMiddleware,
@@ -20,32 +21,34 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# class 
-MODEL = tf.keras.models.load_model("tomatoes2.h5")
+MODEL = load_model("tomatoes2.h5")
 
 CLASS_NAMES = ["Others", "Tomato_healthy", "Tomato_mosaic_virus"]
 
 
-
 def read_file_as_image(data) -> np.ndarray:
-    image = np.array(Image.open(BytesIO(data)))
+    image = Image.open(BytesIO(data))
+    image = image.resize((256, 256))
+    image = image.convert("RGB")  # Convert to RGB if the input image has a different mode
+    image = np.array(image)
     return image
 
+
 @app.post("/predict")
-async def predict(
-    file: UploadFile = File(...)
-):
+async def predict(file: UploadFile = File(...)):
+    #file_extension = file.filename.split(".")[-1]
     image = read_file_as_image(await file.read())
+
     img_batch = np.expand_dims(image, 0)
-    
     predictions = MODEL.predict(img_batch)
 
     predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
     confidence = np.max(predictions[0])
     return {
-        'class': predicted_class,
-        'confidence': float(confidence)
+        "class": predicted_class,
+        "confidence": float(confidence),
     }
 
+
 if __name__ == "__main__":
-    uvicorn.run(app, host='localhost', port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
